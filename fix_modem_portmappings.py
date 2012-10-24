d88916b254f32f6929479a5cab414450de2512a8
Casper Kuijjer <casper@kuijjer.com>
2012-10-24 20:50:30 +0200

    Playing around with getting the ip address and creating urls

---
+++ fix_modem_portmappings.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import socket
+
+modem_ip = '192.168.1.1'
+
+add_format = 'http://%(modem_ip)s/scvrtsrv.cmd?action=add&srvName=%(server_ip)s&srvAddr=%(server_ip)s&proto=1,&eStart=%(port)s,&eEnd=%(port)s,&iStart=%(port)s,&iEnd=%(port)s'
+
+remove_format = 'http://%(modem_ip)s/scvrtsrv.cmd?action=remove&rmLst=%(server_ip_int)s|%(port)s|%(port)s|1|0|0|%(port)s|%(port)s,'
+
+def get_ipaddress():
+    return socket.gethostbyname(socket.gethostname())
+
+def convert_ipaddress_to_int(ipaddress):
+    return int(socket.inet_pton(socket.AF_INET, ipaddress).encode('hex'), 16)
+
+if __name__ == '__main__':
+    server_ip = get_ipaddress();
+    server_ip_int = convert_ipaddress_to_int(server_ip)
+    port = 80
+
+    values = { 
+        'modem_ip': modem_ip,
+        'server_ip': server_ip,
+        'server_ip_int': server_ip_int,
+        'port': port,
+    }
+
+    print convert_ipaddress_to_int('192.168.1.22')
+    print get_ipaddress()
+
+    print values
+    print add_format % values
