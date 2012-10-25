3b378718ac4b87102230423436d9628b896d29a1
Casper Kuijjer <casper@kuijjer.com>
2012-10-25 19:55:36 +0200

    Added a PortMappingHandler that is able to get and set portmappings

--- fix_modem_portmappings.py
+++ fix_modem_portmappings.py
@@ -1,41 +1,105 @@
 #!/usr/bin/python
 
 import socket
+import collections
 import mechanize
+import BeautifulSoup
 
-modem_ip = '192.168.1.1'
 
-modem_url = 'http://%(modem_ip)s/scvrtsrv.cmd?action=a'
+PortMapping = collections.namedtuple('PortMapping', 'name ip port')
 
-view_url = modem_url + '=view'
-add_url = modem_url + '=add&srvName=%(server_ip)s&srvAddr=%(server_ip)s&proto=1,&eStart=%(port)s,&eEnd=%(port)s,&iStart=%(port)s,&iEnd=%(port)s'
-remove_url = modem_url + '=remove&rmLst=%(server_ip_int)s|%(port)s|%(port)s|1|0|0|%(port)s|%(port)s,'
+class PortMappingHandler():
+    def __init__(self):
+        options = {
+            'modem_ip': '192.168.1.1',
+            'username': 'user',
+            'password': 'user',
+        }
+
+        self.base_url = 'http://%s/scvrtsrv.cmd?action=' % options['modem_ip']
+        self.view_url = self.base_url + '=view'
+        self.append_url = self.base_url + '=add&srvName=%(name)s&srvAddr=%(ip)s&proto=1,&eStart=%(port)s,&eEnd=%(port)s,&iStart=%(port)s,&iEnd=%(port)s'
+        self.remove_url = self.base_url + '=remove&rmLst=%(ip_int)s|%(port)s|%(port)s|1|0|0|%(port)s|%(port)s,'
+
+        self.browser = mechanize.Browser()
+        self.browser.add_password(self.base_url, options['username'], options['password'])
+
+    def retrieve(self):
+        self.browser.open(self.view_url)
+        soup = BeautifulSoup.BeautifulSoup(self.browser.response.read())
+
+        # the first table row is a header
+        rows = soup.findAll('tr')[1:]
+        mappings = []
+        for row in rows:
+            cells = row.findAll('td')
+            mappings.append(PortMapping(
+                    name = cells[0].string, 
+                    ip = cells[6].string,
+                    port = cells[1].string))
+
+        return mappings
+
+    def append(self, mapping):
+        print 'adding %s %s %s' % mapping
+        return
+        self.browser.open(self.append_url % {
+                'name': mapping.name,
+                'ip': mapping.ip,
+                'port': mapping.port,
+            })
+
+    def remove(self, mapping):
+        print 'removing %s %s %s' % mapping
+        return
+        self.browser.open(self.remove_url % {
+                'ip_int': ip_to_int(mapping.ip),
+                'port': mapping.port,
+            })
+
+    def ip_to_int(self, ipaddress):
+        return int(socket.inet_pton(socket.AF_INET, ipaddress).encode('hex'), 16)
+
 
 def get_ipaddress():
     return socket.gethostbyname(socket.gethostname())
 
-def convert_ipaddress_to_int(ipaddress):
-    return int(socket.inet_pton(socket.AF_INET, ipaddress).encode('hex'), 16)
+def main():
+    ip = get_ipaddress();
+
+    wanted_mappings = [
+        PortMapping(name='ssh', ip=ip, port=22),
+        PortMapping(name='http', ip=ip, port=80),
+        PortMapping(name='https', ip=ip, port=443),
+    ]
+
+    handler = PortMappingHandler()
+    existing_mappings = handler.retrieve()
+
+    to_remove = []
+    to_add = []
+
+    for wanted in wanted_mappings:
+        wanted_found = False
+
+        for existing in existing_mappings:
+            if wanted.port == existing.port:
+                found = True
+
+            if wanted.port == existing.port and wanted.ip != existing.ip:
+                to_remove.append(existing)
+                to_add.append(wanted)
+
+        if not wanted_found:
+            mappings_to_add.append(wanted)
+
+
+    for mapping in to_remove:
+        handler.remove(mapping)
+
+    for mapping in to_add:
+        handler.append(mapping)
+
 
 if __name__ == '__main__':
-    server_ip = get_ipaddress();
-    server_ip_int = convert_ipaddress_to_int(server_ip)
-    port = 80
-
-    values = { 
-        'modem_ip': modem_ip,
-        'server_ip': server_ip,
-        'server_ip_int': server_ip_int,
-        'port': port,
-    }
-
-    print convert_ipaddress_to_int('192.168.1.22')
-    print get_ipaddress()
-
-    print values
-    print add_url % values
-
-    br = mechanize.Browser()
-    br.add_password(modem_url % values, 'user', 'user')
-    br.open(view_url % values)
-    print br.response().read()
+    main()
